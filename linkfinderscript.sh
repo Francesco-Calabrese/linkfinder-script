0b8ee6e9f6041011c88d031a868fdbb0c17e7732
Francesco-Calabrese <96243400+Francesco-Calabrese@users.noreply.github.com>
2024-12-24 09:54:11 -0500

    Add files via upload

--- linkfinderscript.sh
+++ linkfinderscript.sh
@@ -2,26 +2,38 @@
 
 function process_line(){
 	echo "line: $1"
-	python linkfinder.py -i $1 --output cli >> output_linkfinderscript
+
+	if [ "$2" = "cli" ]; then
+		python linkfinder.py -i $1 --output cli >> output_linkfinderscript
+	else
+		python linkfinder.py -i $1 
+	fi
 }
 
-if [ $# -ne 1 ]; then
-	echo "Usage $0 <list of urls>"
+if [ $# -ne 2 ]; then
+	echo "Usage $0 <list of urls> <options - either cli or gui>"
 	exit 1
 fi
 
+if [ "$2" = "cli" ]; then
+	echo -e "Printing output to the file: output_linkfinderscript\n"
+	echo "" > output_linkfinderscript
+else
+	echo -e "Opening new items found in browser\n"
+fi
+
+
 line_num=0
+array_hashes=()
 
-echo -e "Printing output to the file: output_linkfinderscript\n"
-echo "" > output_linkfinderscript
-
-printf "Total lines in file: %d\n\n" "$(wc -l $1)"
+words="$(wc -l $1)"
+printf "Total lines in file: %s\n\n" "${words}"
 
 while read line; do
 	((line_num++))
 	printf "$line_num "
-	process_line "$line"
-	sleep 2
+	process_line "$line" $2
+	sleep 3
 done < $1
 
 sort output_linkfinderscript | uniq > output_sorted
